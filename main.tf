179c77ef895cf1b284a7088a9b091df77e3e2c8f
Sagar Thorat <sagar.thorat@cenitex.vic.gov.au>
2025-02-27 12:52:00 +1100

    userdata

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.this.id
   instance_type = "t3.micro"
-
+  user_data = filebase64("/scripts/userdata.sh")
   tags = {
     Name = "HelloWorld"
   }
