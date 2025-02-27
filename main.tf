c1201efedb67e641a4f6fd69814d98ba9df9eff4
Sagar Thorat <sagar.thorat@cenitex.vic.gov.au>
2025-02-27 13:39:22 +1100

    lifecycle

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ resource "aws_instance" "webserver" {
   ami           = data.aws_ami.this.id
   instance_type = "t2.medium"
   user_data = filebase64("scripts/userdata.sh")
+  lifecycle {
+    replace_triggered_by = [
+      # Replace `aws_appautoscaling_target` each time this instance of
+      # the `aws_ecs_service` is replaced.
+      aws_instance.webserver.id
+    ]
+  }
   tags = {
     Name = "WebServer"
   }
