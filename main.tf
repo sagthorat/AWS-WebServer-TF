d10ae8a3966ba2099ac580713160786037a7f571
Sagar Thorat <sagar.thorat@cenitex.vic.gov.au>
2025-02-27 14:23:31 +1100

    removed trigger

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ resource "aws_instance" "webserver" {
   ami           = data.aws_ami.this.id
   instance_type = "t2.medium"
   user_data     = filebase64("scripts/userdata.sh")
-  lifecycle {
-    replace_triggered_by = [
+#   lifecycle {
+#     replace_triggered_by = [
 
-      data.aws_ami.this.id
+#       data.aws_ami.this.id
 
-    ]
-  }
+#     ]
+#   }
   tags = {
     Name = "WebServer001"
   }
