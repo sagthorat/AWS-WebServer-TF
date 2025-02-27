f7b872ba35f78dcecd4ff3c91debabf05484607f
Sagar Thorat <sagar.thorat@cenitex.vic.gov.au>
2025-02-27 16:43:17 +1100

    SG String

--- sg.tf
+++ sg.tf
@@ -15,7 +15,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   from_port   = 80
   to_port     = 80
   ip_protocol = "tcp"
-  cidr_ipv4   = ["0.0.0.0/0"]
+  cidr_ipv4   = "0.0.0.0/0"
 
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
@@ -24,7 +24,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   from_port   = 22
   to_port     = 22
   ip_protocol = "tcp"
-  cidr_ipv4   = ["0.0.0.0/0"]
+  cidr_ipv4   = "0.0.0.0/0"
 
 }
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
